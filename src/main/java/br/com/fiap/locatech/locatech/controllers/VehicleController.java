75483254721567d7ace97ffe525cc7fac54f770f
Clebson Moura <clebson.sh@gmail.com>
2025-05-09 21:33:28 -0300

    add route to get all vehicles

--- src/main/java/br/com/fiap/locatech/locatech/controllers/VehicleController.java
+++ src/main/java/br/com/fiap/locatech/locatech/controllers/VehicleController.java
@@ -1,15 +1,35 @@
 package br.com.fiap.locatech.locatech.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.fiap.locatech.locatech.entities.Vehicle;
+import br.com.fiap.locatech.locatech.repositories.VehicleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/vehicles")
 public class VehicleController {
+    private static final Logger logger = LoggerFactory.getLogger(VehicleController.class);
+
+    private final VehicleRepository vehicleRepository;
+
+    public VehicleController(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
 
     @GetMapping
-    public String getVehicles() {
-        return "Hello World";
+    public ResponseEntity<List<Vehicle>> findAll(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size
+    ) {
+        logger.info("/vehicles");
+        List<Vehicle> vehicles = this.vehicleRepository.findAll(page, size);
+
+        return ResponseEntity.ok().body(vehicles);
     }
 }
