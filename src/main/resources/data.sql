5ff7420a9ea38cb9a8179158a06aa4e6e5c3d36f
Clebson Moura <clebson.sh@gmail.com>
2025-05-10 20:34:00 -0300

    add people table and seeds a person

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,3 +11,15 @@ CREATE TABLE vehicles
 
 INSERT INTO vehicles (make, model, plate, production_year, color, daily_rent)
 VALUES ('Chevrolet', 'Camaro', 'TMJ-2K42', 2025, 'yellow', 500.00);
+
+CREATE TABLE people
+(
+    id    BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name  VARCHAR(255),
+    cpf   VARCHAR(255),
+    phone VARCHAR(255),
+    email VARCHAR(255)
+);
+
+INSERT INTO people (name, cpf, phone, email)
+VALUES ('John Doe', '11122233345', '11988776655', 'john.doe@mail.com')
