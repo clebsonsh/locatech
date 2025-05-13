cc431dc275c6e671853ba294e8ab47f5dd639928
Clebson Moura <clebson.sh@gmail.com>
2025-05-13 14:07:08 -0300

    add handler to validation errors

--- src/main/java/br/com/fiap/locatech/locatech/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/br/com/fiap/locatech/locatech/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,17 @@
 package br.com.fiap.locatech.locatech.controllers.handlers;
 
 import br.com.fiap.locatech.locatech.dtos.ResourceNotFoundDTO;
+import br.com.fiap.locatech.locatech.dtos.ValidationErrorDTO;
 import br.com.fiap.locatech.locatech.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -15,5 +21,17 @@ public class ControllerExceptionHandler {
         var status = HttpStatus.NOT_FOUND;
 
         return ResponseEntity.status(status.value()).body(new ResourceNotFoundDTO(e.getMessage(), status.value()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var status = HttpStatus.BAD_REQUEST;
+        List<String> errors = new ArrayList<>();
+
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            errors.add(fieldError.getField() + ": " + fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status.value()).body(new ValidationErrorDTO(errors, status.value()));
     }
 }
